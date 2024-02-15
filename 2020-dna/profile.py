0e466890fe9d44672672985e0f8abd3fa05c1a1e
samarth <samarthkulkarni@icloud.com>
2024-02-15 23:18:35 +0530

    working

--- 2020-dna/profile.py
+++ 2020-dna/profile.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+import re
 
 data = []
 with open(sys.argv[1], "r")as file:
@@ -11,5 +12,28 @@ sequence = ''
 with open(sys.argv[2], "r") as file:
     sequence = file.read()
 
-print(data)
-print(sequence)
+occurrences = [0] * (len(data[0])-1)
+
+for i,_ in enumerate(data[0][1:]):
+    pattern = data[0][i+1]
+    regex_pattern = f"(?:{pattern})+"
+    matches = re.findall(regex_pattern, sequence)
+    if matches:
+        longest_match = max(matches, key=len)
+        occurrences[i] = len(longest_match)//len(pattern)
+    else:
+        occurrences[i] = 0
+
+
+for row_idx, row in enumerate(data[1:]):
+    match = True
+    for col_idx in range(1, len(row)):
+        if int(row[col_idx]) != occurrences[col_idx - 1]:
+            match = False
+            break
+    
+    if match:
+        print(data[row_idx+1][0])
+
+
+
